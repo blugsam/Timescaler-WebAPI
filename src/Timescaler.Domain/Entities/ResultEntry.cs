42bae5c19132e283de25dcb4c291dd971f5f516b
blugsam <mijiyarow.leonid@gmail.com>
2025-07-19 16:48:20 +0300

    domain layer

---
+++ src/Timescaler.Domain/Entities/ResultEntry.cs
@@ -0,0 +1,14 @@
+﻿namespace Timescaler.Domain.Entities
+{
+    public class ResultEntry
+    {
+        public Guid Id { get; set; }
+        public string FileName { get; set; } = string.Empty;
+        public DateTime FirstOperationDate { get; set; }
+        public double AverageExecutionTime { get; set; }
+        public double AverageValue { get; set; }
+        public double MedianValue { get; set; }
+        public double MaxValue { get; set; }
+        public double MinValue { get; set; }
+    }
+}
---
+++ src/Timescaler.Domain/Entities/ValueEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace Timescaler.Domain.Entities
+{
+    public class ValueEntry
+    {
+        public Guid Id { get; set; }
+        public string SourceFileName { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public double ExecutionTime { get; set; }
+        public double Value { get; set; }
+    }
+}
