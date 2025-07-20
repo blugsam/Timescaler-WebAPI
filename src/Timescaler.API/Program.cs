2dc25107ef5c53dc6950aa76843d9e42e5fb192e
blugsam <mijiyarow.leonid@gmail.com>
2025-07-20 14:00:09 +0300

    pre migrations db context registration

--- src/Timescaler.API/Program.cs
+++ src/Timescaler.API/Program.cs
@@ -1,23 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Timescaler.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
-// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddDbContext<TimescalerDbContext>
+    (options =>
+    {
+        options.UseNpgsql(configuration.GetConnectionString(nameof(TimescalerDbContext)));
+    });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
