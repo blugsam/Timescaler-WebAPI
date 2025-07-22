ef5f75ca46543750535e54a1831d5c261c839e7f
blugsam <mijiyarow.leonid@gmail.com>
2025-07-23 01:18:21 +0300

    date format specification

--- src/Timescaler.Application/Validation/CsvRowParser.cs
+++ src/Timescaler.Application/Validation/CsvRowParser.cs
@@ -5,6 +5,8 @@ namespace Timescaler.Application.Validation;
 
 public class CsvRowParser : ICsvRowParser
 {
+    private const string CustomDateTimeFormat = "yyyy-MM-ddTHH-mm-ss.ffffZ";
+
     public bool TryParse(RawCsvRow rawRow, out ParsedCsvRow? parsedRow, out string? parseError)
     {
         parsedRow = null;
@@ -12,9 +14,15 @@ public class CsvRowParser : ICsvRowParser
 
         var fields = rawRow.Fields;
 
-        if (!DateTime.TryParse(fields[0], CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out var date))
+        //if (!DateTime.TryParse(fields[0], CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out var date))
+        //{
+        //    parseError = "Неверный формат даты.";
+        //    return false;
+        //}
+
+        if (!DateTime.TryParseExact(fields[0], CustomDateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out var date))
         {
-            parseError = "Неверный формат даты.";
+            parseError = "Неверный формат даты. Ожидался формат 'yyyy-MM-ddTHH-mm-ss.ffffZ'.";
             return false;
         }
 
