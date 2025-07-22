8a4a11bcce4458bcf8115f34518fe8a4c2eee14f
blugsam <mijiyarow.leonid@gmail.com>
2025-07-22 23:34:40 +0300

    dependency injected

--- src/Timescaler.API/Program.cs
+++ src/Timescaler.API/Program.cs
@@ -1,17 +1,43 @@
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using Timescaler.Application.Ports;
+using Timescaler.Application.Services;
+using Timescaler.Application.Services.Interfaces;
+using Timescaler.Application.Services.Parsing;
+using Timescaler.Application.Validation;
 using Timescaler.Infrastructure.Data;
+using Timescaler.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddDbContext<TimescalerDbContext>
-    (options =>
-    {
-        options.UseNpgsql(configuration.GetConnectionString(nameof(TimescalerDbContext)));
-    });
+builder.Services.AddDbContext<TimescalerDbContext>(options =>
+{
+    options.UseNpgsql(configuration.GetConnectionString(nameof(TimescalerDbContext)));
+});
+
+builder.Services.AddScoped<IResultRepository, ResultRepository>();
+builder.Services.AddScoped<IRawValueRepository, RawValueRepository>();
+
+builder.Services.AddScoped<IDataProcessingService, DataProcessingService>();
+builder.Services.AddScoped<ICsvParser, CsvParser>();
+
+builder.Services.AddScoped<IFileValidator, FileValidator>();
+builder.Services.AddScoped<ICsvRowParser, CsvRowParser>();
+
+builder.Services.AddScoped<IValidator<RawCsvRow>, CsvStructureValidator>();
+builder.Services.AddScoped<IValidator<ParsedCsvRow>, CsvBusinessRulesValidator>();
+builder.Services.AddScoped<IValidator<FileStructure>, FileStructureValidator>();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
