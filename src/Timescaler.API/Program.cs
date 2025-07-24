fef25308e0e19cee900f4b8c3877753b3118065d
blugsam <mijiyarow.leonid@gmail.com>
2025-07-24 14:09:12 +0300

    serilog logging added

--- src/Timescaler.API/Program.cs
+++ src/Timescaler.API/Program.cs
@@ -1,47 +1,68 @@
-using FluentValidation;
 using Microsoft.EntityFrameworkCore;
-using Timescaler.Domain.Repositories;
+using FluentValidation;
+using Serilog;
 using Timescaler.Application.Services;
 using Timescaler.Application.Services.Interfaces;
 using Timescaler.Application.Services.Parsing;
 using Timescaler.Application.Validation;
+using Timescaler.Domain.Repositories;
 using Timescaler.Infrastructure.Data;
 using Timescaler.Infrastructure.Repositories;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-var configuration = builder.Configuration;
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<TimescalerDbContext>(options =>
+try
 {
-    options.UseNpgsql(configuration.GetConnectionString(nameof(TimescalerDbContext)));
-});
+    var builder = WebApplication.CreateBuilder(args);
+    var configuration = builder.Configuration;
 
-builder.Services.AddScoped<IResultRepository, ResultRepository>();
-builder.Services.AddScoped<IRawValueRepository, RawValueRepository>();
+    builder.Host.UseSerilog((context, services, loggerConfiguration) => loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext());
 
-builder.Services.AddScoped<IDataProcessingService, DataProcessingService>();
-builder.Services.AddScoped<ICsvParser, CsvParser>();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IFileValidator, FileValidator>();
-builder.Services.AddScoped<ICsvRowParser, CsvRowParser>();
+    builder.Services.AddDbContext<TimescalerDbContext>(options =>
+    {
+        options.UseNpgsql(configuration.GetConnectionString(nameof(TimescalerDbContext)));
+    });
 
-builder.Services.AddScoped<IValidator<RawCsvRow>, CsvStructureValidator>();
-builder.Services.AddScoped<IValidator<ParsedCsvRow>, CsvBusinessRulesValidator>();
-builder.Services.AddScoped<IValidator<FileStructure>, FileStructureValidator>();
+    builder.Services.AddScoped<IResultRepository, ResultRepository>();
+    builder.Services.AddScoped<IRawValueRepository, RawValueRepository>();
 
-var app = builder.Build();
+    builder.Services.AddScoped<IDataProcessingService, DataProcessingService>();
+    builder.Services.AddScoped<ICsvParser, CsvParser>();
+    builder.Services.AddScoped<IFileValidator, FileValidator>();
+    builder.Services.AddScoped<ICsvRowParser, CsvRowParser>();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddScoped<IValidator<RawCsvRow>, CsvStructureValidator>();
+    builder.Services.AddScoped<IValidator<ParsedCsvRow>, CsvBusinessRulesValidator>();
+    builder.Services.AddScoped<IValidator<FileStructure>, FileStructureValidator>();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.MapControllers();
-
-app.Run();
+catch(Exception exception)
+{
+    Log.Fatal(exception, "App terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
